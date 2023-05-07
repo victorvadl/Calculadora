197ffd3055539e32f959e2a66eaf1089f3adbe71
Victor Rafael <victor.codes@hotmail.com>
2023-05-07 11:51:54 -0300

    Update Program.cs

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ObjetoCalculadora.Models;
+
+Calculadora calc = new Calculadora();
+calc.Somar(3,5);
+calc.Dividir(10,2);
+calc.Multiplicar(10,5);
+calc.Subtrair(50,20);
+calc.Potencia(2,10);
+calc.Seno(35);
+calc.Coseno(60);
+calc.Tangente(90);
+calc.RaizQuadrada(9);
